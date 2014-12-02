fa205970f963d76483a719674109d0a9b19f1a2d
Alexander Volochnev <a.volochnev@gmail.com>
2014-12-02 23:59:38 +0300

    Docblocs added

--- Exception/JsonSchemaException.php
+++ Exception/JsonSchemaException.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the json-schema bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HadesArchitect\JsonSchemaBundle\Exception;
 
 class JsonSchemaException extends \RuntimeException
