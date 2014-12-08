d58f5801f338b797c22e31035184a14050e9107f
Alexander Volochnev <a.volochnev@gmail.com>
2014-12-09 02:27:30 +0300

    Annotations added

--- Uri/UriResolverService.php
+++ Uri/UriResolverService.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the json-schema bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HadesArchitect\JsonSchemaBundle\Uri;
 
 use JsonSchema\Uri\UriResolver as BaseResolver;
--- Validator/ValidatorService.php
+++ Validator/ValidatorService.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of the json-schema bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace HadesArchitect\JsonSchemaBundle\Validator;
 
 use HadesArchitect\JsonSchemaBundle\Error\Error;
